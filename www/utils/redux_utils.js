759aeb917c5ac31563ef4c5705f28fd23bb2af83
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2018-02-26 03:37:27 -0500

    Addded observers.

--- www/utils/redux_utils.js
+++ www/utils/redux_utils.js
@@ -1,8 +1,11 @@
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import {observer, observe} from 'redux-observers';
 import * as ActionCreators from '../store/action_creators';
 
 class ReduxUtils {
+  static _observers = [];
+
   static connect (mapStateToProps, shouldMapDispatchToProps=false) {
     return connect(mapStateToProps, shouldMapDispatchToProps ? this.mapDispatchToProps : null);
   };  
@@ -16,6 +19,14 @@ class ReduxUtils {
       let {actions} = this.mapDispatchToProps(dispatch);
       observerFunc(actions, currentState, previousState);
     };
+  };
+
+  static registerObserver (mapStateToPropsFunc, observerFunc, options) {
+    this._observers.push(observer(mapStateToPropsFunc, this.dispatchFunc(observerFunc), options));
+  };
+
+  static observeChanges (store, options) {
+    observe(store, this._observers, options);
   };
 };
 
