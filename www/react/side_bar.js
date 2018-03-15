f7bbc75ebed39344c893cc3cd4cb5b1bfb59a2b3
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2018-03-15 06:42:46 -0400

    Used the editor components.

--- www/react/side_bar.js
+++ www/react/side_bar.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import EditorButton from './editor_button'
+import ReduxUtils from '../utils/redux_utils';
+import EditorButton from './editor_button';
+import WallsEditor from './walls_editor';
+import FloorsEditor from './floors_editor';
 
-let SideBar = () => (
-  <div id='side-bar'>
-    <EditorButton text='Wall'  type='walls'  />
-    <EditorButton text='Floor' type='floors' />
-  </div>
-);
 
-export default SideBar;
+class SideBar extends React.Component {
+  getEditorComponent () {
+    if (this.props.editor === 'walls')
+      return (<WallsEditor />);
+    if (this.props.editor === 'floors')
+      return (<FloorsEditor />);
+    return null;
+  };
+
+  render () {
+    let editor = this.getEditorComponent();
+    return (
+      <div id='side-bar'>
+        <EditorButton text='Wall'  type='walls'  />
+        <EditorButton text='Floor' type='floors' />
+        {editor}
+      </div>
+    );
+  };
+};
+
+let mapStateToProps = (state, ownProps) => {
+  return {
+    editor: state.editor,
+  };
+};
+
+export default ReduxUtils.connect(mapStateToProps, true)(SideBar);
