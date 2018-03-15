4ec538af5c924153a1daa18b3d24e6672360af98
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2018-03-15 05:31:05 -0400

    Removed editor classes (will be redone as react components).

--- www/react/side_bar.js
+++ www/react/side_bar.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import EditorButton from './editor_button'
-import WallsEditor from '../editor/walls_editor';
-import FloorsEditor from '../editor/floors_editor';
 
 let SideBar = () => (
   <div id="side-bar">
-    <EditorButton text="Wall"  editor={WallsEditor} />
-    <EditorButton text="Floor" editor={FloorsEditor} />
+    <EditorButton text="Wall"  />
+    <EditorButton text="Floor" />
   </div>
 );
 
