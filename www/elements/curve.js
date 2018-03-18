5cb36bf81f065402fe9b39ea979725aad0c58477
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2018-03-18 14:55:36 -0400

    Chnged curve color to balck. Used width to distinguish permanent vs temporary elements.

--- www/elements/curve.js
+++ www/elements/curve.js
@@ -8,8 +8,9 @@ class Curve {
 
   render (temp) {
     let {start, end} = this;
-    let color = temp ? 'blue' : 'red';
-    return (<line x1={start.x} y1={start.y} x2={end.x} y2={end.y} stroke={color} strokeWidth='2' vectorEffect='non-scaling-stroke' />);
+    let color = 'black';
+    let width = temp ? 1 : 2;
+    return (<line x1={start.x} y1={start.y} x2={end.x} y2={end.y} stroke={color} strokeWidth={width} vectorEffect='non-scaling-stroke' />);
   };
 };
 
