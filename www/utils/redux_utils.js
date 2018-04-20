fb1e8c23ad9e0d59cbdf3e6e180e076aef08eb64
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2018-04-20 01:57:20 -0400

    Removed redux-observers.

--- www/utils/redux_utils.js
+++ www/utils/redux_utils.js
@@ -1,11 +1,8 @@
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {observer, observe} from 'redux-observers';
 import * as ActionCreators from '../store/action_creators';
 
 class ReduxUtils {
-  static _observers = [];
-
   static connect (mapStateToProps, shouldMapDispatchToProps=false) {
     return connect(mapStateToProps, shouldMapDispatchToProps ? this.mapDispatchToProps : null);
   };  
@@ -19,14 +16,6 @@ class ReduxUtils {
       let {actions} = this.mapDispatchToProps(dispatch);
       observerFunc(actions, currentState, previousState);
     };
-  };
-
-  static registerObserver (mapStateToPropsFunc, observerFunc, options) {
-    this._observers.push(observer(mapStateToPropsFunc, this.dispatchFunc(observerFunc), options));
-  };
-
-  static observeChanges (store, options) {
-    observe(store, this._observers, options);
   };
 };
 
