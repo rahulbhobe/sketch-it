9578ef0afc9c08f1086d693b48dcfc361e11f03c
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2018-05-07 07:05:06 -0400

    Added isUrl() implmentation.

--- www/utils/request_utils.js
+++ www/utils/request_utils.js
@@ -1,16 +1,19 @@
 import request from 'request';
+import isUrl from 'is-url';
 import {promisify} from 'es6-promisify';
 
 
 class RequestUtils {
   static getRequest (route, payload) {
-    return promisify(request)({url: window.location.origin + route, qs: payload}).then((httpResponse) => {
+    let url = isUrl(route) ? route : window.location.origin + route;
+    return promisify(request)({url, qs: payload}).then((httpResponse) => {
       return JSON.parse(httpResponse.body);
     });
   };
 
   static postRequest (route, payload) {
-    return promisify(request.post)({url: window.location.origin + route, json: payload}).then((httpResponse) => {
+    let url = isUrl(route) ? route : window.location.origin + route;
+    return promisify(request.post)({url, json: payload}).then((httpResponse) => {
       return httpResponse.body;
     });
   };
